37db599dad070e028206a0f427c5b3117e7a9d37
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-27 23:00:30 +0530

    Added eks action list in data.tf

--- data.tf
+++ data.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "eks_assume_role_policy" {
 }
 
 # Create the policy which allows other actions for the EKS instance
-data "aws_iam_policy_document" "eks_assume_role_policy" {
+data "aws_iam_policy_document" "eks_access_role_policy" {
   statement {
     actions = [
       "eks:ListFargateProfiles",
