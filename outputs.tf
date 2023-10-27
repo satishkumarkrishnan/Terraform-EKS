91f42df88c871994eef9a4a6455114d394d14139
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-27 21:34:15 +0530

    Added EKS role,policy,policy attachment

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "endpoint" {
+/*output "endpoint" {
   value = aws_eks_cluster.tokyo_EKS.endpoint
   depends_on = [aws_eks_cluster.tokyo_EKS.name]
 }
@@ -7,4 +7,4 @@ output "kubeconfig-certificate-authority-data" {
   #value = aws_eks_cluster.tokyo_EKS.certificate_authority[0].data
   value = aws_eks_cluster.tokyo_EKS.certificate_authority[0].data
   depends_on = [aws_eks_cluster.tokyo_EKS.name]
-}
+}*/
