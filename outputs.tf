ec6c4c068cd4a1a6ee29ed2778cfdf43ce1e4b6c
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-27 21:39:48 +0530

    Added depends on condition

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "endpoint" {
   value = aws_eks_cluster.tokyo_EKS.endpoint
-  depends_on = [aws_eks_cluster.tokyo_EKS.name]
+  depends_on = [aws_eks_cluster.tokyo_EKS]
 }
 
 output "kubeconfig-certificate-authority-data" {
   #value = aws_eks_cluster.tokyo_EKS.certificate_authority[0].data
   value = aws_eks_cluster.tokyo_EKS.certificate_authority[0].data
-  depends_on = [aws_eks_cluster.tokyo_EKS.name]
+  depends_on = [aws_eks_cluster.tokyo_EKS]
 }
