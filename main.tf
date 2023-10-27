d5457c3bb816b48e5224a8ad6e51d76a150aa4ee
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-27 12:53:37 +0530

    Added IAM and VPC  conf files

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ terraform {
     }
   }
 }
+
+#To use the VPC module already created
+module "rds" {
+  source    = "git@github.com:satishkumarkrishnan/terraform-aws-vpc.git?ref=main"   
+}
+
+#To use the IAM module already created
+module "iam" {
+  source    = "git@github.com:satishkumarkrishnan/Terraform_IAM.git?ref=main"
+}
