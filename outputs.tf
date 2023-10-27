f36d802ff23334328a4cc5acea5461f5bc8fe97d
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-27 21:50:48 +0530

    Removed value commnet

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,6 @@ output "endpoint" {
 }
 
 output "kubeconfig-certificate-authority-data" {
-  #value = aws_eks_cluster.tokyo_EKS.certificate_authority[0].data
   value = aws_eks_cluster.tokyo_EKS.certificate_authority[0].data
   depends_on = [aws_eks_cluster.tokyo_EKS]
 }
