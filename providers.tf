fc4a2ba20ec6c985df03b722ea8c92749e6c9112
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-03-12 12:12:07 +0530

    added a provider config for kubernetes

--- providers.tf
+++ providers.tf
@@ -11,3 +11,11 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+provider "kubernetes" {
+    alias                  = "aws"
+    host                   = data.terraform_remote_state.kubernetes.outputs.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.kubernetes.outputs.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.default.token
+    #load_config_file       = false
+  }
